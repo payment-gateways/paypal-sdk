edbc1c7f8fe3a71770fa675d2b5dcfbe1ca1cf42
Darío Rivera <fermius.us@gmail.com>
2021-03-16 20:24:34 -0500

    test: add unit tests for ShippingAddress

---
+++ tests/Unit/Subscriptions/ShippingAddressTest.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace PaymentGateway\PayPalSdk\Tests\Unit\Subscriptions;
+
+use PaymentGateway\PayPalSdk\Subscriptions\Constants\CountryCode;
+use PaymentGateway\PayPalSdk\Subscriptions\ShippingAddress;
+use PaymentGateway\PayPalSdk\Subscriptions\ShippingDetailAddressPortable;
+use PaymentGateway\PayPalSdk\Subscriptions\ShippingDetailName;
+use PaymentGateway\PayPalSdk\Tests\TestCase;
+
+class ShippingAddressTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function itGeneratesAnEmptyArrayWithNoData()
+    {
+        $shippingAddress = new ShippingAddress();
+
+        $this->assertSame([], $shippingAddress->toArray());
+    }
+
+    /**
+     * @test
+     */
+    public function itGeneratesAnArrayArrayWithName()
+    {
+        $shippingAddress = new ShippingAddress();
+        $shippingName = new ShippingDetailName($this->faker->name);
+        $shippingAddress->setName($shippingName);
+
+        $this->assertSame(['name' => $shippingName->toArray()], $shippingAddress->toArray());
+    }
+
+    /**
+     * @test
+     */
+    public function itGeneratesAnArrayArrayWithAddress()
+    {
+        $shippingAddress = new ShippingAddress();
+        $shippingDetailAddressPortable = new ShippingDetailAddressPortable(CountryCode::ARGENTINA);
+        $shippingAddress->setAddress($shippingDetailAddressPortable);
+
+        $this->assertSame(['address' => $shippingDetailAddressPortable->toArray()], $shippingAddress->toArray());
+    }
+
+    /**
+     * @test
+     */
+    public function itCanChangeItsData()
+    {
+        $shippingAddress = new ShippingAddress();
+        $shippingName = new ShippingDetailName($this->faker->name);
+        $shippingAddress->setName($shippingName);
+        $shippingDetailAddressPortable = new ShippingDetailAddressPortable(CountryCode::ARGENTINA);
+        $shippingAddress->setAddress($shippingDetailAddressPortable);
+
+        $this->assertSame($shippingName, $shippingAddress->getName());
+        $this->assertSame($shippingDetailAddressPortable, $shippingAddress->getAddress());
+
+        $shippingName = new ShippingDetailName($this->faker->name);
+        $shippingAddress->setName($shippingName);
+        $shippingDetailAddressPortable = new ShippingDetailAddressPortable(CountryCode::COLOMBIA);
+        $shippingAddress->setAddress($shippingDetailAddressPortable);
+
+        $this->assertSame($shippingName, $shippingAddress->getName());
+        $this->assertSame($shippingDetailAddressPortable, $shippingAddress->getAddress());
+    }
+}
