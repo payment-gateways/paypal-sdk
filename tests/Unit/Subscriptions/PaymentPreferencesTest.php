e4198ab2ad80f482f604468f9b1212fe4877361a
Darío Rivera <fermius.us@gmail.com>
2021-03-16 19:40:31 -0500

    test: add unit test for PaymentPreferences

---
+++ tests/Unit/Subscriptions/PaymentPreferencesTest.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace PaymentGateway\PayPalSdk\Tests\Unit\Subscriptions;
+
+use PaymentGateway\PayPalSdk\Subscriptions\Constants\CurrencyCode;
+use PaymentGateway\PayPalSdk\Subscriptions\Constants\InitialPaymentFailureAction;
+use PaymentGateway\PayPalSdk\Subscriptions\Money;
+use PaymentGateway\PayPalSdk\Subscriptions\PaymentPreferences;
+use PHPUnit\Framework\TestCase;
+
+class PaymentPreferencesTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function itGeneratesAnEmptyArrayWithNotData()
+    {
+        $paymentPreferences = new PaymentPreferences();
+
+        $this->assertSame([
+            'auto_bill_outstanding' => true,
+            'setup_fee_failure_action' => 'CANCEL',
+            'payment_failure_threshold' => 0
+        ], $paymentPreferences->toArray());
+    }
+
+    /**
+     * @test
+     */
+    public function itGeneratesAnEmptyArrayWithSetupFee()
+    {
+        $paymentPreferences = new PaymentPreferences();
+        $paymentPreferences->setSetupFee(new Money(CurrencyCode::AUSTRALIAN_DOLLAR, '12'));
+
+        $this->assertSame([
+            'auto_bill_outstanding' => true,
+            'setup_fee_failure_action' => 'CANCEL',
+            'payment_failure_threshold' => 0,
+            'setup_fee' => [
+                'currency_code' => 'AUD',
+                'value' => '12'
+            ]
+        ], $paymentPreferences->toArray());
+    }
+
+    /**
+     * @test
+     */
+    public function itCanChangeItsData()
+    {
+        $paymentPreferences = new PaymentPreferences();
+        $money = new Money(CurrencyCode::AUSTRALIAN_DOLLAR, '12');
+        $paymentPreferences->setSetupFee($money);
+
+        $this->assertTrue($paymentPreferences->isAutoBillOutstanding());
+        $this->assertSame(0, $paymentPreferences->getPaymentFailureThreshold());
+        $this->assertSame(InitialPaymentFailureAction::CANCEL, $paymentPreferences->getSetupFeeFailureAction());
+        $this->assertSame($money, $paymentPreferences->getSetupFee());
+
+        $money = new Money(CurrencyCode::UNITED_STATES_DOLLAR, '120');
+        $paymentPreferences->setSetupFee($money);
+        $paymentPreferences->setAutoBillOutstanding(false);
+        $paymentPreferences->setPaymentFailureThreshold(1);
+        $paymentPreferences->setSetupFeeFailureAction(InitialPaymentFailureAction::CONTINUE);
+
+        $this->assertFalse($paymentPreferences->isAutoBillOutstanding());
+        $this->assertSame(1, $paymentPreferences->getPaymentFailureThreshold());
+        $this->assertSame(InitialPaymentFailureAction::CONTINUE, $paymentPreferences->getSetupFeeFailureAction());
+        $this->assertSame($money, $paymentPreferences->getSetupFee());
+    }
+}
