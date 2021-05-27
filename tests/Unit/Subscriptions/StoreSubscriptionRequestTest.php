0f646e8fe1cb7d746ffa9975fb09b7f91e71e6f8
Darío Rivera <fermius.us@gmail.com>
2021-05-26 21:07:55 -0500

    test: improve test with faker and more values

--- tests/Unit/Subscriptions/StoreSubscriptionRequestTest.php
+++ tests/Unit/Subscriptions/StoreSubscriptionRequestTest.php
@@ -4,9 +4,10 @@ namespace PaymentGateway\PayPalSdk\Tests\Unit\Subscriptions;
 
 use PaymentGateway\PayPalSdk\Subscriptions\ApplicationContext;
 use PaymentGateway\PayPalSdk\Subscriptions\PayerName;
+use PaymentGateway\PayPalSdk\Subscriptions\Phone;
 use PaymentGateway\PayPalSdk\Subscriptions\Requests\StoreSubscriptionRequest;
 use PaymentGateway\PayPalSdk\Subscriptions\Subscriber;
-use PHPUnit\Framework\TestCase;
+use PaymentGateway\PayPalSdk\Tests\TestCase;
 
 class StoreSubscriptionRequestTest extends TestCase
 {
@@ -25,21 +26,26 @@ class StoreSubscriptionRequestTest extends TestCase
      */
     public function itCreatesRequestsWithSubscriberData()
     {
+        $subscriberFirstName = $this->faker->firstName;
+        $subscriberLastName = $this->faker->lastName;
+        $emailAddress = $this->faker->address;
+        $payerId = $this->faker->uuid;
+
         $request = new StoreSubscriptionRequest('P-18T532823A424032WL7NIVUA');
         $subscriber = new Subscriber();
         $name = new PayerName();
-        $name->setGivenName('John Doe');
+        $name->setGivenName($subscriberFirstName);
+        $name->setSurname($subscriberLastName);
         $subscriber->setName($name);
+        $subscriber->setEmailAddress($emailAddress);
+        $subscriber->setPayerId($payerId);
+        $subscriber->setPhone(new Phone($this->faker->phoneNumber));
         $request->setSubscriber($subscriber);
 
         $this->assertSame(
             [
-            'plan_id' => 'P-18T532823A424032WL7NIVUA',
-            'subscriber' => [
-                'name' => [
-                    'given_name' => 'John Doe'
-                ]
-            ]
+                'plan_id' => 'P-18T532823A424032WL7NIVUA',
+                'subscriber' => $subscriber->toArray()
             ],
             $request->toArray()
         );
@@ -56,11 +62,11 @@ class StoreSubscriptionRequestTest extends TestCase
 
         $this->assertSame(
             [
-            'plan_id' => 'P-18T532823A424032WL7NIVUA',
-            'application_context' => [
-                'return_url' => 'http://example.com/return-url',
-                'cancel_url' => 'http://example.com/cancel-url',
-            ]
+                'plan_id' => 'P-18T532823A424032WL7NIVUA',
+                'application_context' => [
+                    'return_url' => 'http://example.com/return-url',
+                    'cancel_url' => 'http://example.com/cancel-url',
+                ]
             ],
             $request->toArray()
         );
